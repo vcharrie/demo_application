a55e6bf8ac8e5318b8938d8cdcf5d4a72cd81735
vincent charrier <vcharrie@gmail.com>
2026-04-01 11:21:38 +0200

    Revert "modify HealthControllerTest"
    This reverts commit 23a506c0176b45f23eda9876c09ede4bded9644d.

--- src/test/java/com/coreservice/api/HealthControllerTest.java
+++ src/test/java/com/coreservice/api/HealthControllerTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -12,8 +11,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
-@AutoConfigureMockMvc(addFilters = false)
-@ActiveProfiles("test")
+@AutoConfigureMockMvc
 class HealthControllerTest {
 
     @Autowired
