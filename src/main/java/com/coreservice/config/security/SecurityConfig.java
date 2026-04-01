6fabd8b35937ca8e7b2f1a292eef0fc1e7e52b69
vincent charrier <vcharrie@gmail.com>
2026-04-01 11:21:38 +0200

    Revert "add modification of security config filter to allow /actuator/health"
    This reverts commit c10d90e6e464b8dc20599c6aa827bb4eb8882460.

--- src/main/java/com/coreservice/config/security/SecurityConfig.java
+++ src/main/java/com/coreservice/config/security/SecurityConfig.java
@@ -20,7 +20,7 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/health","/actuator/health").permitAll()
+                        .requestMatchers("/api/health").permitAll()
                         .anyRequest().authenticated()
                 );
 
