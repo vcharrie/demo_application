618857a73340ed1da4af3741589ffc0df46c2f0f
vcharrie <vcharrie@gmail.com>
2026-04-25 22:12:54 +0200

    change spring boot version in 3.2.5 LTS to fix security vulnerabilities in a few spring boot dependencies when analysing container image for vulnerabilities with trivy:

--- src/test/java/com/coreservice/exception/GlobalExceeptionHandlerTest.java
+++ src/test/java/com/coreservice/exception/GlobalExceeptionHandlerTest.java
@@ -1,9 +1,8 @@
 package com.coreservice.exception;
 
 import org.junit.jupiter.api.Test;
-
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.beans.factory.annotation.Autowired;
 
