ecef4f8a9580ecb127f8d5908eae9b97f8c06d92
vcharrie <vcharrie@gmail.com>
2026-04-25 22:12:54 +0200

    change spring boot version in 3.2.5 LTS to fix security vulnerabilities in a few spring boot dependencies when analysing container image for vulnerabilities with trivy:

--- src/main/java/com/coreservice/config/security/SecurityConfig.java
+++ src/main/java/com/coreservice/config/security/SecurityConfig.java
@@ -3,11 +3,13 @@ package com.coreservice.config.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
